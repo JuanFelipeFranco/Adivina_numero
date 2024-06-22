d7795c89e93ef3d697ccfc449aa2973d1a1a526c
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-06-22 15:59:52 -0500

    update - refactor

--- Adivina_Numero/app.js
+++ Adivina_Numero/app.js
@@ -1,11 +1,18 @@
 //DOM : DOCUMENT OBJECT MODEL
 //document nos permite trabajar de manera dinamica entre javascript y html; es un objeto donde le vamos a poner un texto.
-let titulo = document.querySelector('h1'); 
-titulo.innerHTML= 'Juego del número secreto';
+function asignarTextoElemento(elemento,texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto; 
+}
 
+/*let titulo = document.querySelector('h1'); 
+titulo.innerHTML= 'Juego del número secreto';
 let parrafo = document.querySelector('p');
-parrafo.innerHTML='indica un numero del 1 al 10';
+parrafo.innerHTML='indica un numero del 1 al 10';*/
 
 function intentoDeUsuario(){
     alert('click')
 }
+
+asignarTextoElemento('h1','Juego del numero secreto')
+asignarTextoElemento('p','indica un numero del 1 al 100')
