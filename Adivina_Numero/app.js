a5c918a75b44150dac34ee8bcd52969a2eb20853
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-06-22 16:14:29 -0500

    add function generate random_number

--- Adivina_Numero/app.js
+++ Adivina_Numero/app.js
@@ -2,9 +2,11 @@
 //document nos permite trabajar de manera dinamica entre javascript y html; es un objeto donde le vamos a poner un texto.
 function asignarTextoElemento(elemento,texto){
     let elementoHTML = document.querySelector(elemento);
-    elementoHTML.innerHTML = texto; 
+    elementoHTML.innerHTML = texto;
+    return;
 }
-
+asignarTextoElemento('h1','Juego del numero secreto')
+asignarTextoElemento('p','indica un numero del 1 al 100')
 /*let titulo = document.querySelector('h1'); 
 titulo.innerHTML= 'Juego del número secreto';
 let parrafo = document.querySelector('p');
@@ -12,7 +14,11 @@ parrafo.innerHTML='indica un numero del 1 al 10';*/
 
 function intentoDeUsuario(){
     alert('click')
+    return;
 }
 
-asignarTextoElemento('h1','Juego del numero secreto')
-asignarTextoElemento('p','indica un numero del 1 al 100')
+let numeroSecreto = generarNumeroSecreto();
+console.log(numeroSecreto);
+function generarNumeroSecreto() {
+    return numeroSecreto = Math.floor(Math.random()*10)+1;
+}
